21a7f38cb3cca8cdd5ce484c80d937eb0c73e35f
eldbxor <eldbxor@naver.com>
2017-04-10 19:41:15 +0900

    compile Poi library in build.gradle and remove poi-ooxml library because of a crash

--- app/src/main/java/com/example/taek/blescanner_bylyt/Utils/ExcelWriter.java
+++ app/src/main/java/com/example/taek/blescanner_bylyt/Utils/ExcelWriter.java
@@ -4,11 +4,6 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import java.util.List;
 
 /**
@@ -32,22 +27,5 @@ public class ExcelWriter {
 
         // 리스트의 size 만큼 row를 생성
 
-    }
-
-    public void xlsxWriter(List list) {
-        // create a workbook
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        // create a worksheet
-        XSSFSheet sheet = workbook.createSheet();
-        // create a row
-        XSSFRow row = sheet.createRow(0);
-        // create a cell;
-        XSSFCell cell;
-
-        // 헤더 정보 구성
-        cell = row.createCell(0);
-        cell.setCellValue("비콘 주소");
-
-        // 리스트의 size 만큼 row를 생성
     }
 }
