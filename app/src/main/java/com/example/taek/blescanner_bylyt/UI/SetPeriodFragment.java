8f000ee817175301ba79896595333ecf01a2cd11
eldbxor <eldbxor@naver.com>
2017-04-21 21:26:18 +0900

    update fragment_about.xml

--- app/src/main/java/com/example/taek/blescanner_bylyt/UI/SetPeriodFragment.java
+++ app/src/main/java/com/example/taek/blescanner_bylyt/UI/SetPeriodFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.taek.blescanner_bylyt.R;
 
@@ -16,6 +19,7 @@ import com.example.taek.blescanner_bylyt.R;
 public class SetPeriodFragment extends Fragment {
     private View rootView;
     Context context_mainActivity;
+    RadioButton rbLowLatency, rbBalanced, rbLowPower, rbOpportunistic;
 
     public SetPeriodFragment() {}
 
@@ -31,6 +35,33 @@ public class SetPeriodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_set_period, container, false);
+
+        rbLowLatency = (RadioButton) rootView.findViewById(R.id.radioButton_LowLatency);
+        rbBalanced = (RadioButton) rootView.findViewById(R.id.radioButton_Balanced);
+        rbLowPower = (RadioButton) rootView.findViewById(R.id.radioButton_LowPower);
+        rbOpportunistic = (RadioButton) rootView.findViewById(R.id.radioButton_Opportunistic);
+
+        RadioButton.OnClickListener optionOnClickListener = new RadioButton.OnClickListener() {
+            public void onClick(View v) {
+                if (rbLowLatency.isChecked()) {
+                    Toast.makeText(context_mainActivity, "Low Latency is checked", Toast.LENGTH_SHORT).show();
+                }
+                else if (rbBalanced.isChecked()) {
+                    Toast.makeText(context_mainActivity, "Balanced is checked", Toast.LENGTH_SHORT).show();
+                }
+                else if (rbLowPower.isChecked()) {
+                    Toast.makeText(context_mainActivity, "Low Power is checked", Toast.LENGTH_SHORT).show();
+                }
+                else if (rbOpportunistic.isChecked()) {
+                    Toast.makeText(context_mainActivity, "Opportunistic is checked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        rbLowLatency.setOnClickListener(optionOnClickListener);
+        rbBalanced.setOnClickListener(optionOnClickListener);
+        rbLowPower.setOnClickListener(optionOnClickListener);
+        rbOpportunistic.setOnClickListener(optionOnClickListener);
+
 
         return rootView;
     }
